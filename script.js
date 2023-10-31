a43429146be9068e3fdc686f837db4c6426c225f
bryant tunbutr <bryantt23@users.noreply.github.com>
2023-10-31 04:12:27 -0700

    add remove

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const myLibrary = [
+let myLibrary = [
   { title: 'Jane Eyre', author: 'Charlotte Bronte', pages: 300 },
   { title: 'Jurassic Park', author: 'Michael Crichton', pages: 200 }
 ];
@@ -7,7 +7,12 @@ function Book(title, author, pages) {
   this.title = title;
   this.author = author;
   this.pages = pages;
+  this.hasBeenRead = false;
 }
+
+// Book.prototype.toggle = status => {
+//   hasBeenRead = status;
+// };
 
 function addBookToLibrary(book) {
   myLibrary.push(book);
@@ -18,11 +23,22 @@ const theForm = document.querySelector('.the-form');
 
 function displayBooks() {
   books.innerHTML = '';
-  for (const book of myLibrary) {
+  for (const [index, book] of myLibrary.entries()) {
     const li = document.createElement('li');
     li.textContent = `Title: ${book.title} by ${book.author}`;
+    // li.setAttribute('index', index);
     books.appendChild(li);
+    const button = document.createElement('button');
+    button.innerText = 'Remove';
+    button.addEventListener('click', () => removeBook(index));
+    li.appendChild(button);
   }
+}
+
+function removeBook(index) {
+  myLibrary = myLibrary.filter((book, i) => i != index);
+
+  displayBooks();
 }
 
 displayBooks();
