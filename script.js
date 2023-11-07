014ee63851155d888f86f892227cb4c318a4cb7c
bryant tunbutr <bryantt23@users.noreply.github.com>
2023-11-07 03:53:52 -0800

    refactor to classes

--- script.js
+++ script.js
@@ -1,60 +1,60 @@
-let myLibrary = [
-  new Book('Jane Eyre', 'Charlotte Bronte', 300, true),
-  new Book('Jurassic Park', 'Michael Crichton', 200)
-];
-
-function Book(title, author, pages, read = false) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
+class Book {
+  constructor(title, author, pages, read = false) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
+  toggle = function () {
+    this.read = !this.read;
+  };
 }
 
-Book.prototype.toggle = function () {
-  this.read = !this.read;
-};
-
-function addBookToLibrary(book) {
-  myLibrary.push(book);
+class Library {
+  constructor() {
+    this.myLibrary = [
+      new Book('Jane Eyre', 'Charlotte Bronte', 300, true),
+      new Book('Jurassic Park', 'Michael Crichton', 200)
+    ];
+    this.displayBooks();
+  }
+  addBookToLibrary(book) {
+    this.myLibrary.push(book);
+    this.displayBooks();
+  }
+  removeBook(index) {
+    this.myLibrary = this.myLibrary.filter((_, i) => i != index);
+    this.displayBooks();
+  }
+  displayBooks() {
+    books.innerHTML = '';
+    for (const [index, book] of this.myLibrary.entries()) {
+      const li = document.createElement('li');
+      li.textContent = `Title: ${book.title} by ${book.author}`;
+      books.appendChild(li);
+      const button = document.createElement('button');
+      button.innerText = 'Remove';
+      button.addEventListener('click', () => this.removeBook(index));
+      li.appendChild(button);
+      const readButton = document.createElement('button');
+      readButton.innerText = book.read ? 'Have read' : 'Have not read';
+      readButton.addEventListener('click', () => {
+        book.toggle();
+        this.displayBooks();
+      });
+      li.appendChild(readButton);
+    }
+  }
 }
 
 const books = document.querySelector('.books');
 const theForm = document.querySelector('.the-form');
-
-function displayBooks() {
-  books.innerHTML = '';
-  for (const [index, book] of myLibrary.entries()) {
-    const li = document.createElement('li');
-    li.textContent = `Title: ${book.title} by ${book.author}`;
-    books.appendChild(li);
-    const button = document.createElement('button');
-    button.innerText = 'Remove';
-    button.addEventListener('click', () => removeBook(index));
-    li.appendChild(button);
-    const readButton = document.createElement('button');
-    readButton.innerText = book.read ? 'Have read' : 'Have not read';
-    readButton.addEventListener('click', () => {
-      book.toggle();
-      displayBooks();
-    });
-    li.appendChild(readButton);
-  }
-}
-
-function removeBook(index) {
-  myLibrary = myLibrary.filter((book, i) => i != index);
-
-  displayBooks();
-}
-
-displayBooks();
+const theLibrary = new Library();
 
 theForm.addEventListener('submit', e => {
   e.preventDefault();
   const title = document.querySelector('.title');
   const author = document.querySelector('.author');
   const pages = document.querySelector('.pages');
-  addBookToLibrary(new Book(title.value, author.value, pages.value));
-
-  displayBooks();
+  theLibrary.addBookToLibrary(new Book(title.value, author.value, pages.value));
 });
